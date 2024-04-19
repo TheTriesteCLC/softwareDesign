fcd14a9da0bdd907d567eefe3f4bd5c741389596
TheTriesteCLC <148186018+TheTriesteCLC@users.noreply.github.com>
2024-04-19 15:49:01 +0700

    Setting handlebars

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require("./src/app");
 const PORT = process.env.PORT || 3001;
 
 const server = app.listen(PORT, () => {
-  console.log(`start project....${PORT}`);
+  console.log(`Listening on http://localhost:${PORT}/`);
 });
--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
+const path = require("path");
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const { engine } = require("express-handlebars");
+const handleBars = require("express-handlebars");
 
 const morgan = require("morgan");
 const helmet = require("helmet");
@@ -19,10 +20,20 @@ app.use(morgan("dev")); // Change color status in terminal
 app.use(helmet()); // Hidden infomation of website
 app.use(compression()); // Decrease load data
 
-// setup handlebars
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./views");
+//Template engine
+app.engine(
+  "hbs",
+  handleBars.engine({
+    extname: ".hbs",
+    helpers: {
+      sum: (a, b) => a + b,
+      json: (content) => JSON.stringify(content),
+      prodClassFromName: (prodName) => prodName.replaceAll(" ", "-"),
+    },
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
 
 //set up when using
 //app.use('/static', express.static(`${__dirname}\\public`))
@@ -34,19 +45,23 @@ require("./dbs/mongo.db");
 app.use("", require("./routers"));
 
 // handle error
-app.use((req, res, next) => {
-  const error = new Error("Not Found");
-  error.status = 404;
-  next(error);
-});
+// app.use((req, res, next) => {
+//   const error = new Error("Not Found");
+//   error.status = 404;
+//   next(error);
+// });
 
-app.use((error, req, res, next) => {
-  const statusCode = error.status || 500;
-  return res.status(statusCode).json({
-    status: "error",
-    code: statusCode,
-    message: error.message || "Internal Server Error",
-  });
+// app.use((error, req, res, next) => {
+//   const statusCode = error.status || 500;
+//   return res.status(statusCode).json({
+//     status: "error",
+//     code: statusCode,
+//     message: error.message || "Internal Server Error",
+//   });
+// });
+
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
 module.exports = app;
