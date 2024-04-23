a03ce6ac03cc53b774f94e1ba768e4f92987d5fe
Long Vu <vucongleo@gmail.com>
2024-04-23 20:07:47 +0700

    Build: ui for home driver

--- src/routers/driver/index.js
+++ src/routers/driver/index.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const siteController = require('../../controllers/driver.c');
 
 router.get('/login', siteController.login);
+router.get('/home', (req, res) => {
+    res.render( 'driver/home', { layout: 'driver/main' });   
+})
 
 module.exports = router;
